68635c11073ea29515134512871db6d1f6654c55
HX <122723004+Cantro93@users.noreply.github.com>
2025-07-18 14:16:05 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const BotChecker = require('./botcheck');
 
-global.bc = new BotChecker(30, '');
+global.bc = new BotChecker(300, '');
 
 const server = http.createServer((req,res) => {
   let data = '';
@@ -13,19 +13,7 @@ const server = http.createServer((req,res) => {
     switch (auth_result.state) {
       case 'accept':
         res.writeHead(200, {'Content-Type': 'text/html', 'Location': 'index.html'});
-        res.end(`
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-            <meta charset="UTF-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-            <title>Document</title>
-          </head>
-          <body>
-            This would be normal page.
-          </body>
-          `);
+        res.end(require('fs').fsReadFileSync('index.html', 'utf8'));
         break;
       case 'reject':
         res.writeHead(403, {'Content-Type': 'text/plain'});
