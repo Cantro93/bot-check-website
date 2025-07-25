1abb57aa4f58b9a4180e94bce57a58a7578c4737
HX <122723004+Cantro93@users.noreply.github.com>
2025-07-25 09:39:05 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const BotChecker = require('./botcheck');
+
+global.bc = new BotChecker(300, '');
+
+const server = http.createServer((req,res) => {
+  let data = '';
+  req.on('data', chunk => {data += chunk.toString();});
+  req.on('end', () => {
+    console.log(`Content type: ${req.headers['content-type']}`);
+    console.log(`Received: ${data}`);
+    const auth_result = global.bc.auth(req, data, false);
+    switch (auth_result.state) {
+      case 'accept':
+        res.writeHead(200, {'Content-Type': 'text/html', 'Location': 'index.html'});
+        res.end(require('fs').fsReadFileSync('internal-index.html', 'utf8'));
+        break;
+      case 'reject':
+        res.writeHead(403, {'Content-Type': 'text/plain'});
+        res.end("Bots are blocked. Retry is possible after 5 minutes.");
+        break;
+      case 'do_auth':
+        res.writeHead(200, {'Content-Type': 'text/html', 'Location': 'verify.html'});
+        res.end(auth_result.body);
+        break;
+      default:
+        break;
+    }
+  });
+});
+
+server.listen(80, '0.0.0.0', () => {console.log("server started")});
